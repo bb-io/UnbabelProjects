71873741d3d6bb219566c1bbfcaaa75518ba340c
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-09 15:50:51 +0300

    added categories

--- Apps.UnbabelProjects/UnbabelProjectsApplication.cs
+++ Apps.UnbabelProjects/UnbabelProjectsApplication.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.UnbabelProjects;
 
-public class UnbabelProjectsApplication : IApplication
+public class UnbabelProjectsApplication : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.MachineTranslationAndMtqe];
+        set { }
+    }
+    
     public string Name
     {
         get => "Unbabel Projects";
